f637a76884998663348eac56a33224d9e253a513
chris_jpark <chrisj.park@mail.utoronto.ca>
2024-09-24 17:11:03 -0400

    edit activity 1.3 to include time

--- hello.py
+++ hello.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template
-app = Flask(__name__)
-
+from flask_moment import Moment
 from flask_bootstrap import Bootstrap
+from datetime import datetime, timezone
 
+
+app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print(datetime.now(timezone.utc))
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time=datetime.now(timezone.utc))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
